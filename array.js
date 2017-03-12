a88b49c2ccb6911f47ef7874750a35854a67afb2
fan222 <sfwahahaha222@gmail.com>
2017-03-11 19:52:27 -0800

    array

--- array.js
+++ array.js
@@ -447,3 +447,31 @@ var searchInsert = function(nums, target) {
         return left;
     }
 };
+
+// 118. Pascal's Triangle
+var generate = function(numRows) {
+    var result = [];
+
+    if(numRows <= 0){
+        return result;
+    }
+
+    for(var i = 0; i < numRows; i++){
+        var cur = [];
+        var pre = i > 0 ? result[i - 1] : [];
+
+        for(var j = 0; j <= i; j++){
+            if(j === 0){
+                cur.push(1);
+            } else if(j === i){
+                cur.push(1);
+            } else {
+                cur.push(pre[j] + pre[j-1]);
+            }
+        }
+
+        result.push(cur);
+    }
+
+    return result;
+};
